8dfddd95514cc9b063a54c3412d8043a5a645a24
BM <86118827+brmarmello@users.noreply.github.com>
2021-11-08 19:15:43 -0300

    Solução dos exercícios de fixação 1, 2 e 3 do dia 4.2 JS.

---
+++ modulo-01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -0,0 +1,33 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Exercício 1
+/*
+for (i = 0; i < numbers.length; i += 1) {
+  console.log(numbers[i]);
+} 
+*/
+
+// Exercício 2
+/*
+let resultado = 0 ;
+
+for (i=0; i < numbers.length; i+=1) {
+  resultado += numbers[i];
+}
+
+console.log(resultado);
+*/
+
+// Exercício 3
+/*
+let media = 0;
+
+for (i=0; i < numbers.length; i+=1) {
+  media += (numbers[i] / numbers.length);
+}
+
+console.log(media);
+*/
+
+// Exercício 4
+
