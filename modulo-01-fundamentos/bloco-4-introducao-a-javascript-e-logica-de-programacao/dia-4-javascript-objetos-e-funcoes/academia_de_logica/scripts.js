81dbe585bc4d10c1cdbc85a747a0114990a0451e
BM <86118827+brmarmello@users.noreply.github.com>
2021-11-10 23:37:52 -0300

    Atualizando arquivos da aula ao vivo, do momento assíncrono e dos exercícios de fixação do dia 4.4.

---
+++ modulo-01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/academia_de_logica/scripts.js
@@ -0,0 +1,20 @@
+/*
+04 - Escreva um nome N vezes
+Escreva um algoritmo usando a estrutura "FOR" que irá receber um nome e a quantidade de vezes que o mesmo deve ser repetido e retorná-lo.
+Por exemplo:
+-------------------------------
+Entradas:
+"João", 5
+Saída:
+"João, João, João, João, João."
+-------------------------------
+*/
+
+function repeatName(name, quantity) {
+  // Desenvolva seu código nessa função
+  for (let i = 0; i < quantity - 1; i += 1) {
+    armazena.push
+  }
+}
+
+module.exports = repeatName
