ea1c0205628fc3339600d4633e145c8bb21b572c
BM <86118827+brmarmello@users.noreply.github.com>
2021-11-18 20:11:08 -0300

    Subindo resolução dos exercícios sincronos do dia 5.3.

--- modulo-01-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicios-assincronos/main.js
+++ modulo-01-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicios-assincronos/main.js
@@ -1,3 +1,6 @@
+// Parte II - Conheça o addEventListener
+
+
 const firstLi = document.getElementById('first-li')
 const secondLi = document.getElementById('second-li')
 const thirdLi = document.getElementById('third-li')
@@ -10,13 +13,29 @@ const myWebpage = document.getElementById('my-spotrybefy')
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+function handleChangeTech(event) {
+  const techElement = document.querySelector('.tech')
+  techElement.classList.remove('tech')
+  event.target.classList.add('tech')
+  input.value = ''
+}
+
+firstLi.addEventListener('click', handleChangeTech)
+secondLi.addEventListener('click', handleChangeTech)
+thirdLi.addEventListener('click', handleChangeTech)
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+input.addEventListener('input', function(event) {
+  const techElement = document.querySelector('.tech');
+  techElement.innerText = event.target.value;
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+myWebpage.addEventListener('dblclick', function() {
+window.location.replace('https://blog.betrybe.com/');
+});
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
@@ -35,3 +54,10 @@ firstLi.addEventListener('dblclick', resetText)
 // Não precisa passar o parâmetro dentro da callback resetText. O próprio
 // navegador fará esse trabalho por você, não é legal? Desse jeito, o
 // event.target na nossa função retornará o objeto 'firstLi'.
+myWebpage.addEventListener('mouseover', function(event) {
+  event.target.style.color = 'red';
+});
+
+myWebpage.addEventListener('mouseout', function(event) {
+  event.target.style.color = 'unset';
+});
