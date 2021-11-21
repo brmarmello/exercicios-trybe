88832396ccb10f794d9dd88eac4b5c5035128650
BM <86118827+brmarmello@users.noreply.github.com>
2021-11-21 17:44:20 -0300

    Resolução da questão 2 - exercícios de ficação - dos exercícios do dia 5.3.

--- modulo-01-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicios-fixacao/script.js
+++ modulo-01-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicios-fixacao/script.js
@@ -69,3 +69,19 @@ function createDaysOfTheMonth() {
 }
 // Agora só falta chamar a função para que ela seja executada.
 createDaysOfTheMonth()
+
+// Exercício 2:
+// Implemente uma função que receba como parâmetro a string "Feriados" e crie dinamicamente um botão com o nome "Feriados".
+// Adicione a este botão a ID "btn-holiday" .
+// Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
+function createHolidayButton(buttonName) {
+  let buttonContainer = document.querySelector('.buttons-container')
+  let newButton = document.createElement('button')
+  let newButtonID = 'btn-holiday'
+
+  newButton.innerHTML = buttonName
+  newButton.id = newButtonID
+  buttonContainer.appendChild(newButton)
+}
+
+createHolidayButton('Feriados')
