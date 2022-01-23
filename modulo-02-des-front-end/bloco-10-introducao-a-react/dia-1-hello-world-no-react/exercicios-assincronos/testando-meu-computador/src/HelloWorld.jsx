51349950a1c5bc538412dd13865fb0f61d1e6fe6
brmarmello <brmarmello@gmail.com>
2022-01-23 01:19:37 -0300

    Atualizando o dia 10.1.

---
+++ modulo-02-des-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicios-assincronos/testando-meu-computador/src/HelloWorld.jsx
@@ -0,0 +1,22 @@
+/*
+import React from 'react';
+
+class HelloWorld extends React.Component {
+  // CÓD. AQUI.
+}
+*/
+
+// OUUUUUUU...
+
+/*
+import React, { Component } from 'react';
+
+class HelloWorld extends Component {
+  // Função render() serve para exibir alguma coisa na tela.
+  render() {
+    return <span>Hello, World!</span>
+  }
+}
+
+export default HelloWorld;
+*/
